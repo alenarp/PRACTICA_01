d23d813ee42b2704c3512a93c24733ad1360571c
Alena <alena.robles.p@gmail.com>
2025-10-29 13:01:38 +0100

    desplegar menú hamburguesa

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Online");
+let burger = document.querySelector(".burger > i");
+let menu_opt = document.querySelector(".navbar__menu");
+
+// Variable para saber si el menu esta abierto o cerrado
+let isOpen = false;
+
+// Definimos evento click sobre el botón del menú
+burger.addEventListener("click", function(){
+
+    burger.classList.toggle("fa-bars");
+    burger.classList.toggle("fa-times");
+    menu_opt.classList.toggle("menu-show");
+
+});
