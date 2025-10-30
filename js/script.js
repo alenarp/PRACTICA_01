7121a9db859cab5af1755bb9b15a1bc98112a0dc
Alena <alena.robles.p@gmail.com>
2025-10-31 00:39:00 +0100

    navbar js + about aura

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@ burger.addEventListener("click", function(){
     menu_opt.classList.toggle("menu-show");
 
 });
+
+//Navbar al scrollear
+  const header = document.querySelector("header");
+
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+
